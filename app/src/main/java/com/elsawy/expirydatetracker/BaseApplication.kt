ceeae6e0a1831ecf0897513895f9519ad74b6088
Ahmed7Elsawy <ahmedelsawy487@gmail.com>
2022-01-21 21:46:43 +0200

    add Service Locator

--- app/src/main/java/com/elsawy/expirydatetracker/BaseApplication.kt
+++ app/src/main/java/com/elsawy/expirydatetracker/BaseApplication.kt
@@ -3,7 +3,7 @@ package com.elsawy.expirydatetracker
 import android.app.Application
 import com.elsawy.expirydatetracker.data.ProductRepository
 
-class ExpiryDateApplication : Application() {
+class BaseApplication : Application() {
 
    val productRepository: ProductRepository
       get() = ServiceLocator.provideProductRepository(this)
