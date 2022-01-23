969ddba517a0ed813fa653014a3799d4ce111bca
Ahmed7Elsawy <ahmedelsawy487@gmail.com>
2022-01-23 02:13:19 +0200

    change the way we show the date

---
+++ app/src/main/java/com/elsawy/expirydatetracker/utils/DateUtils.kt
@@ -0,0 +1,45 @@
+package com.elsawy.expirydatetracker.utils
+
+import java.text.SimpleDateFormat
+import java.util.*
+
+object DateUtils {
+
+   private const val HOUR_MILLI_SECONDS = 3600000L
+   private const val DAY_MILLI_SECONDS = 86400000L
+
+   @JvmStatic
+   fun dateToString(date: Date): String {
+
+      if (isPastDate(date)) {
+         return "expired at ${formatTheDate(date)}"
+      }
+
+      if (isLessThanOneDay(date)) {
+         val remainHours = getRemainHours(date)
+         return "less than ${getRemainHours(date)} hours"
+      }
+
+      return "expiry date ${formatTheDate(date)}"
+   }
+
+   private fun formatTheDate(date: Date): String {
+      val formatter = SimpleDateFormat("dd/MM/yyyy");
+      return formatter.format(date)
+   }
+
+   private fun getRemainHours(date: Date): Long {
+      val currentDate = System.currentTimeMillis()
+      return (date.time - currentDate) / HOUR_MILLI_SECONDS + 1
+   }
+
+   private fun isPastDate(date: Date): Boolean {
+      val currentDate = System.currentTimeMillis()
+      return currentDate > date.time
+   }
+
+   private fun isLessThanOneDay(date: Date): Boolean {
+      val currentDate = System.currentTimeMillis()
+      return DAY_MILLI_SECONDS >= date.time - currentDate
+   }
+}
